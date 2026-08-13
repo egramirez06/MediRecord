5855cb0b99020c5bff8ada3e48afc6d264a20e9b
Luna <carlosguzmanluna8@gmail.com>
2026-08-13 14:27:19 -0600

    MedicalRecordsList
    se creo el paquete y la clase

---
+++ src/MedicalRecordsList/medicalrecordslist.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MedicalRecordsList;
+
+/**
+ *
+ * @author norki
+ */
+public class medicalrecordslist {
+    
+}
