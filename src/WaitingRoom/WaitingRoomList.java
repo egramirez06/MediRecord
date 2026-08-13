3e387f3f63981c072f092670579d149d7fd31516
Nicoletta <nicoleartavia062@gmail.com>
2026-08-13 15:23:08 -0600

    Se hizo cambio en KeyDynamicslists
    cambio en el nombre de KeyDynamicslists

--- src/WaitingRoom/WaitingRoomList.java
+++ src/WaitingRoom/WaitingRoomList.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package WaitingRoom;
 
-/**
- *
- * @author nicol
- */
-public class WaitingRoomList {
-    
+import clinic.SequentialDynamicsLists;
+import java.util.Iterator;
+import java.util.Queue;
+import patients.Patient;
+
+public class WaitingRoomList implements SequentialDynamicsLists<Patient>{
+        Queue<Patient> WaitingList;
+
+    @Override
+    public Patient get() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean remove() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean add(Patient item) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Iterator getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
--- src/clinic/KeyDynamicsLists.java
+++ src/clinic/KeyDynamicsLists.java
@@ -1,5 +1,5 @@
 package clinic;
-public interface KeyDinamicsLists<T,K> extends DynamicLists<T>{
+public interface KeyDynamicsLists<T,K> extends DynamicLists<T>{
     public T get(K id);
     public boolean remove(K id);
 }
