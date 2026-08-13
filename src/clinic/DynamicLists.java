824fffc110b37685ee7b03f9c5c852b9ae0dcd6f
Mari <mafloresur@est.utn.ac.cr>
2026-08-13 14:14:42 -0600

    Se creo la interfaz DynamicsLists

---
+++ src/clinic/DynamicLists.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package clinic;
+
+/**
+ *
+ * @author maria
+ */
+public interface DynamicLists <T,K> {
+    public boolean add(T item);
+    public int size();
+    public boolean isEmpty();
+}
